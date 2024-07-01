2b4860dd518b0c91e1a4c6a96a1070becca74329
SimoneDUrso <simone.durso98@gmail.com>
2024-07-01 14:51:33 +0200

    [UPD] Created btn_verifica and relative event click

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,7 @@
 const nomiAutorizzati = ["simone d'urso", "laura corrado", "alessandro farina", "gioele micca", "rivaldo gjoni"];
 
+let btn_verifica = document.getElementById("btn-verifica");
+btn_verifica.addEventListener("click", function () {
+
+
+})
