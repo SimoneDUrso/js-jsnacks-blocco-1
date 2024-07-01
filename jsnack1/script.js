a3898e9fd1c3166160cf7c7d51a0a86eb5e13f5c
SimoneDUrso <simone.durso98@gmail.com>
2024-07-01 15:02:40 +0200

    [UPD] Added button "delete" and fixed name of variable. Snack 1 completed.

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,9 +8,25 @@ btn_verifica.addEventListener("click", function () {
 
     for (let i = 0; i < nomiAutorizzati.length; i++) {
 
-        if (nomiAutorizzati[i] === nomiInput) {
+        if (nomiAutorizzati[i] === nomeInput) {
             autorizzato = true;
         }
     }
 
+    // STAMPIAMO I MESSAGGI CORRISPONDENTI
+    if (autorizzato === true) {
+        document.getElementById("message").innerHTML = "Accesso consentito, puoi partecipare!";
+    }
+
+    else {
+        document.getElementById("message").innerHTML = "Accesso negato, per favore non farmi chiamare il buttafuori.";
+    }
+
+})
+
+let btnDelete = document.getElementById("delete")
+btnDelete.addEventListener("click", function () {
+
+    document.getElementById("message").innerHTML = "";
+    document.getElementById("nome").value = "";
 })
