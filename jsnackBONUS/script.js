87b7cf6ff57577282d92d47deadee86051dde0c3
SimoneDUrso <simone.durso98@gmail.com>
2024-07-01 16:25:22 +0200

    [UPD] Created IF to verify that we are actually entering a number and if so it adds the sum and ELSE to give an error message if we don't enter a number.

--- jsnackBONUS/script.js
+++ jsnackBONUS/script.js
@@ -3,4 +3,14 @@ let addition = 0;
 for (let i = 0; i < 10; i++) {
     let numero = (prompt("Inserisci un numero:"));
 
+    numero = parseFloat(numero);
+
+    if (numero === numero) {
+        addition += numero;
+    }
+
+    else {
+        console.log("Il valore inserito non è un numero valido. Verrà ignorato.");
+    }
 }
+
