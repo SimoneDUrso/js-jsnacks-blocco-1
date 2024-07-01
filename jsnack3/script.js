6295fa9d36681c219c1fc4e61db16a1e518aefe2
SimoneDUrso <simone.durso98@gmail.com>
2024-07-01 15:49:01 +0200

    [UPD] Created IF to force the user to enter 4 numbers

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,2 +1,8 @@
 // CHIEDIAMO ALL'UTENTE DI INSERIRE 4 CIFRE NEL PROMPT
 let numero = prompt("Inserisci un numero di 4 cifre:");
+
+// VERIFICHIAMO CHE IL NUMERO SIA DI 4 CIFRE
+if (numero.length !== 4) {
+    document.getElementById("addition").innerText = ("Cifra non valida, inserisci un numero di 4 cifre.")
+}
+
