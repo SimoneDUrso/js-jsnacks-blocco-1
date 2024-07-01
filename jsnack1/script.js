faca6314bfedd4a68aec7418a031bc24f866ee82
SimoneDUrso <simone.durso98@gmail.com>
2024-07-01 14:55:34 +0200

    [UPD] Created two variables "nomeInput" and "autorizzato" and FOR cycle with IF inside him

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,5 +3,14 @@ const nomiAutorizzati = ["simone d'urso", "laura corrado", "alessandro farina", 
 let btn_verifica = document.getElementById("btn-verifica");
 btn_verifica.addEventListener("click", function () {
 
+    let nomeInput = document.getElementById("nome").value;
+    let autorizzato = false;
+
+    for (let i = 0; i < nomiAutorizzati.length; i++) {
+
+        if (nomiAutorizzati[i] === nomiInput) {
+            autorizzato = true;
+        }
+    }
 
 })
