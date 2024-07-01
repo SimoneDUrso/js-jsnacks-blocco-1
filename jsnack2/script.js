559052c815380fd8a284f03d05ddebfeedff7768
SimoneDUrso <simone.durso98@gmail.com>
2024-07-01 15:23:14 +0200

    [UPD] Created FOR cycle with IF inside for ask the user 6 times to enter a number and check if the number is odd

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,2 +1,11 @@
 // CREAZIONE ARRAY VUOTO
 let numeriDispari = [];
+
+for (let i = 0; i < 6; i++) {
+    let numero = parseInt(prompt("Inserisci un numero:"));
+
+    // Controllo se il numero è dispari
+    if (numero % 2 !== 0) {
+        numeriDispari.push(numero);
+    }
+}
